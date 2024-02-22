b7d9b69a6f0a2b4a7197f7d2b27b9ea6d29ff59a
Program Is Fun <134474566+26ProgramIsFun@users.noreply.github.com>
2024-02-22 09:03:27 +0700

    alhamdulillah

---
+++ script.js
@@ -0,0 +1,9 @@
+// Menampilkan formulir yang tersembunyi sebelumnya.
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+// Menyembunyikan formulir yang sedang ditampilkan.
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
